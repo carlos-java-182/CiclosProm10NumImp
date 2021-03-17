e243faf339cc0938f64531b0b77640f1e00a6ada
carlos-java-182 <carlos.villasenor@outlook.com>
2021-03-16 23:23:33 -0600

    Java

---
+++ src/prom10NumImp/NumerosImpares.java
@@ -0,0 +1,27 @@
+package prom10NumImp;
+
+public class NumerosImpares {
+
+	public static void main(String[] args) {
+		
+		long producto=1;
+		
+		
+		for(int i=1; i<=20; i+=2) {
+			
+			
+				producto*=i;
+				System.out.println("Tu qué haces?"+i);
+				
+				
+				
+			}
+			
+			
+		
+		
+		System.out.println("El producto de los 10 primeros impares es: "+producto);
+		
+	}
+
+	}
